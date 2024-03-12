86dd19434086d21a790fbdf373cd8223c574cc36
@hexlet/cli <support@hexlet.io>
2024-03-12 17:32:10 +0300

    submit java-spring-ru/dto-mapper

---
+++ java-spring-ru/dto-mapper/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,60 @@
+package exercise.controller;
+
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.exception.ResourceNotFoundException;
+import exercise.mapper.ProductMapper;
+import exercise.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    // BEGIN
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductDTO> index() {
+        return productRepository.findAll()
+                .stream()
+                .map(productMapper::map)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDTO show(@PathVariable long id) {
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+        return productMapper.map(product);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDTO create(@RequestBody ProductCreateDTO productData) {
+        var product = productMapper.map(productData);
+        productRepository.save(product);
+        return productMapper.map(product);
+    }
+
+    @PutMapping("/{id}")
+    public ProductDTO update(@RequestBody ProductUpdateDTO productData, @PathVariable long id) {
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+        productMapper.update(productData, product);
+        productRepository.save(product);
+        return productMapper.map(product);
+    }
+    // END
+}
